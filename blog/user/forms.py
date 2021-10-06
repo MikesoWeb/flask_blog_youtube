ac82ec6c231fcff5c891c974ca9d981af550fa54
MikesoWeb <klionpsyx@gmail.com>
2021-10-06 16:52:48 +0300

    'add_user_acc'

--- blog/user/forms.py
+++ blog/user/forms.py
@@ -1,6 +1,8 @@
 from flask import flash
+from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
 from wtforms.validators import DataRequired, EqualTo, Length, Email, ValidationError
 from blog.models import User
 
@@ -30,3 +32,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Емайл', validators=[DataRequired(), Email()])
+    picture = FileField('Изображение (png, jpg)', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Обновить')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                flash('Это имя уже занято. Пожалуйста, выберите другое', 'danger')
+                raise ValidationError('That username is taken. Please choose a different one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                flash('Этот емайл уже занят. Пожалуйста, введите другой', 'danger')
+                raise ValidationError('That email is taken. Please choose a different one')
--- blog/user/routes.py
+++ blog/user/routes.py
@@ -2,13 +2,15 @@ import os
 import shutil
 from datetime import datetime
 
-from flask import Blueprint, render_template, flash, url_for
-from flask_login import current_user, logout_user, login_required
+import sqlalchemy
+from flask import Blueprint, render_template, flash, url_for, request
+from flask_login import current_user, logout_user, login_required, login_user
 from werkzeug.utils import redirect
 
 from blog import bcrypt, db
-from blog.models import User
-from blog.user.forms import RegistrationForm
+from blog.models import User, Post
+from blog.user.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from blog.user.utils import save_picture, random_avatar
 
 users = Blueprint('users', __name__)
 
@@ -20,14 +22,16 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password,
+                    image_file=random_avatar(form.username.data))
         db.session.add(user)
         db.session.commit()
 
-        full_path = os.path.join(os.getcwd(), 'blog/static', 'profile_pics', user.username)
-        if not os.path.exists(full_path):
-            os.mkdir(full_path)
-        shutil.copy(f'{os.getcwd()}/blog/static/profile_pics/default.jpg', full_path)
+        # full_path = os.path.join(os.getcwd(), 'blog', 'static', 'profile_pics/', user.username, 'account_img')
+        # if not os.path.exists(full_path):
+        #     os.makedirs(full_path)
+        #
+        # shutil.copy(f'{os.getcwd()}/blog/static/profile_pics/default.jpg', full_path)
         flash('Ваш аккаунт был создан. Вы можете войти на блог', 'success')
         return redirect(url_for('users.login'))
     return render_template('register.html', form=form, title='Регистрация', legend='Регистрация')
@@ -35,16 +39,91 @@ def register():
 
 @users.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'hello'
+    if current_user.is_authenticated:
+        return redirect(url_for('main.blog'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash(f'Вы вошли как пользователь {current_user.username}', 'info')
+            return redirect(next_page) if next_page else redirect(url_for('users.account'))
+        else:
+            flash('Войти не удалось. Пожалуйста, проверьте электронную почту или пароль', 'danger')
+    return render_template('login.html', form=form, title='Логин', legend='Войти')
 
 
 @users.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html')
+    user = User.query.filter_by(username=current_user.username).first()
+    posts = Post.query.all()
+    users = User.query.all()
+    form = UpdateAccountForm()
+
+    if request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    elif form.validate_on_submit():
+        path_one = os.path.join(os.getcwd(), f'blog/static/profile_pics/{user.username}')
+        path_two = os.path.join(os.getcwd(), f'blog/static/profile_pics/{form.username.data}')
+        os.rename(path_one, path_two)
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+
+        if form.picture.data:
+            current_user.image_file = save_picture(form.picture.data)
+        else:
+            form.picture.data = current_user.image_file
+
+        db.session.commit()
+        flash('Ваш аккаунт был обновлён!', 'success')
+        return redirect(url_for('users.account'))
+    image_file = url_for('static',
+                         filename=f'profile_pics/' + current_user.username + '/account_img/' + current_user.image_file)
+    return render_template('account.html', title='Аккаунт',
+                           image_file=image_file, form=form, posts=posts, users=users, user=user)
 
 
-@users.route('/logout')
+@users.get('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user) \
+        .order_by(Post.date_posted.desc()) \
+        .paginate(page=page, per_page=3)
+
+    return render_template('user_posts.html', title='Блог>', posts=posts, user=user)
+
+@users.route('/user_delete/<string:username>', methods=['GET', 'POST'])
+@login_required
+def delete_user(username):
+    try:
+        user = User.query.filter_by(username=username).first_or_404()
+        if user and user.id != 1:
+            db.session.delete(user)
+            db.session.commit()
+            full_path = os.path.join(os.getcwd(), 'blog/static', 'profile_pics', user.username)
+            print(full_path)
+            shutil.rmtree(full_path)
+
+            flash(f'Пользователь {username} был удалён!', 'info')
+            return redirect(url_for('users.account'))
+    except sqlalchemy.exc.IntegrityError:
+        flash(f'У пользователя {username} есть контент!', 'warning')
+        return redirect(url_for('users.account'))
+    except FileNotFoundError:
+        return redirect(url_for('users.account'))
+
+    else:
+        flash('Администрация!', 'info')
+        return redirect(url_for('users.account'))
+
+
+
+@users.get('/logout')
+@login_required
 def logout():
     current_user.last_seen = datetime.now()
     db.session.commit()
